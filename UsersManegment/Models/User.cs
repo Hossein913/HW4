e7837bffa1725b39b8f82ea297106894855afeb1
Hossein913 <Hosssein913@gmail.com>
2023-05-23 16:29:23 +0330

    Add model class

---
+++ UsersManegment/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersManegment.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        //it's just for test
+        public override string ToString()
+        {
+            return $"{id} - {Name} - {PhoneNumber} - {BirthDate} - {CreationDate}";
+        }
+    }
+}
