eec233c6b496e5e6c9fbe7d1c9b0eab2d1bf3b8c
Hossein913 <Hosssein913@gmail.com>
2023-05-24 09:04:17 +0330

    User Insert menu is developing but i have to leave project

--- UsersManegment/Program.cs
+++ UsersManegment/Program.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UsersManegment.DataStorage;
 using UsersManegment.Services;
 
 namespace UsersManegment
 {
     class Program
     {
+        public static DataStorageRepository DSR ;
+        public Program()
+        {
+            DSR = new DataStorageRepository();
+        }
         static void Main(string[] args)
         {
             EntryPoint();
@@ -66,7 +72,123 @@ namespace UsersManegment
 
         private static void NewUser()
         {
-            throw new NotImplementedException();
+            const int idStartPoint = 1000;
+
+            int Id = idStartPoint + DSR.UserCount + 1;
+            string name;
+            string phoneNumber;
+            string birthDate;
+
+            //bool validationFlag = true;
+            bool existingFlag = false;
+
+
+            do
+            {
+                //if (!validationFlag)
+                //{
+                //    Console.WriteLine();
+                //    validationFlag = true;
+
+                //}
+
+                if (existingFlag)
+                {
+                    existingFlag = false;
+                }
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("to Go previous menu write Q");
+                Console.ResetColor();
+
+                Console.WriteLine("Fill out the registeration form:");
+
+
+
+
+                Console.Write("Name: ");
+                name = Console.ReadLine();
+                if (name == "Q")
+                {
+                    EntryPoint();
+                }
+
+                Console.Write(" PhoneNumber: ");
+                phoneNumber = Console.ReadLine();
+                if (phoneNumber == "Q")
+                {
+                    EntryPoint();
+                }
+
+                Console.Write("BitrhDay: ");
+                birthDate = Console.ReadLine();
+                if (birthDate == "Q")
+                {
+                    EntryPoint();
+                }
+
+                //try
+                //{
+                //    validationFlag = Validation.RoleValidation(role.Trim());
+                //}
+                //catch (System.Exception msg)
+                //{
+
+                //    Console.WriteLine(msg.Message);
+                //    validationFlag = false;
+                //}
+
+                //if (validationFlag)
+                //{
+                //        existingFlag = Authenticat.checkUserExist(email, password);
+                //}
+
+                //if (validationFlag && !existingFlag)
+                //{
+                //    reInput = false;
+                //}
+
+                existingFlag = Authenticat.checkUserExist(email, password);
+
+                if (existingFlag)
+                {
+                    Console.WriteLine("Member has registered!");
+                    Thread.Sleep(2000);
+                }
+
+            } while (existingFlag);
+
+            Role curentRole = Role.Member;
+            switch (role)
+            {
+                case "1":
+                    curentRole = Role.Librarian;
+                    break;
+                case "2":
+                    curentRole = Role.Member;
+                    break;
+                default:
+                    break;
+            }
+
+            Person person = new Person(Id, name, email, password, curentRole);
+            try
+            {
+                Authenticat.Register(person);
+                Console.WriteLine("you have Registered ");
+                Thread.Sleep(3000);
+                EntryPoint();
+            }
+            catch (System.Exception msg)
+            {
+
+                Console.WriteLine(msg.Message);
+                Thread.Sleep(3000);
+                RunRegister();
+            }
+
+
+
         }
 
 
