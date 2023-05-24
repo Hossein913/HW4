722190ec421a2bdd9102f5dcadc08ec8d91060a8
Hossein913 <Hosssein913@gmail.com>
2023-05-24 08:51:23 +0330

    EntryPoint of UserInterFace Menu has completed

--- UsersManegment/Program.cs
+++ UsersManegment/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UsersManegment.Services;
 
 namespace UsersManegment
 {
@@ -10,6 +11,80 @@ namespace UsersManegment
     {
         static void Main(string[] args)
         {
+            EntryPoint();
         }
+
+
+        private static void EntryPoint()
+        {
+            bool menuFlag;
+            string menuInput = "";
+
+            Console.Clear();
+            Console.WriteLine(":: Wellcome to UserMenegment system ::");
+            Console.WriteLine();
+            Console.WriteLine(" -> Main Menu ");
+            Console.WriteLine("[1]. Insert new user");
+            Console.WriteLine("[2]. Show useres list");
+            Console.WriteLine("[3]. Edite user info");
+            Console.WriteLine("[4]. Delete user");
+            Console.WriteLine();
+
+            do
+            {
+                Console.Write("Insert menu number:");
+                menuInput = Console.ReadLine();
+                try
+                {
+                    menuFlag = Validation.MenuControl(menuInput, 4);
+                }
+                catch (SystemException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    menuFlag = true;
+                }
+
+            } while (menuFlag);
+
+            switch (menuInput)
+            {
+                case "1":
+                    NewUser();
+                    break;
+                case "2":
+                    ShowUserList();
+                    break;
+                case "3":
+                    EditeUser();
+                    break;
+                case "4":
+                    DeleteUser();
+                    break;
+
+            }
+        }
+
+        private static void NewUser()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        private static void DeleteUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void EditeUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ShowUserList()
+        {
+            throw new NotImplementedException();
+        }
+
+
     }
 }
---
+++ UsersManegment/Services/Validation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersManegment.Services
+{
+    public static class Validation
+    {
+
+        public static bool MenuControl(string input, int endnum)
+        {
+
+            int menuInput;
+            bool flag = false;
+
+            flag = int.TryParse(input.Trim(), out menuInput);
+            if (!flag)
+            {
+                throw new InvalidCastException("Input is not a number");
+            }
+            if (flag = true && (menuInput < 1 || menuInput > endnum))
+            {
+                flag = false;
+                throw new ArgumentOutOfRangeException("input number is out of Range");
+            }
+            return flag;
+
+        }
+
+
+
+    }
+}
